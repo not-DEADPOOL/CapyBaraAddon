08565345a555a3b12effe22080c507fe604762b5
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2023-03-08 19:37:36 +0100

    Update cageify.js

--- src/cageify.js
+++ src/cageify.js
@@ -2,49 +2,35 @@ const INTERVAL_KEY = "INTERVAL_KEY";
 // default to 1s refresh window
 const DEFAULT_INTERVAL = "1000";
 
-const NICS = [
+const NICS = [//I've just replaced Nicolas Cage's images with capybaras 
     "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/1200px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG", // wikipedia Capybara 1
     "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Bristol.zoo.capybara.arp.jpg/1200px-Bristol.zoo.capybara.arp.jpg", // wikipedia Capybara 2
     "https://cdn.britannica.com/77/191677-050-3CBF2834/Capybara.jpg", // enciclopedia Capybara
     "https://inaturalist-open-data.s3.amazonaws.com/photos/91546944/large.jpg", // sitting capy
     "https://s7d2.scene7.com/is/image/TWCNews/akron-zoo-capybara-oh-102522_10252022", // sitting capy2
-    "https://c4.wallpaperflare.com/wallpaper/400/158/569/nicolas-cage-actor-man-face-wallpaper-preview.jpg",
-    "http://www.slicktiger.co.za/wp-content/uploads/2011/11/nic_cage_faceoff11.jpg",
-    "http://4.bp.blogspot.com/_N91xwP-lKvw/TFjSKU-ETZI/AAAAAAAABlc/-mAlrcPNZeI/s400/nickcagevampkiss.jpg", 
-    "https://i1.wp.com/fortworthreport.org/wp-content/uploads/2021/07/PIG001.jpeg",
-    "https://upload.wikimedia.org/wikipedia/commons/c/c0/Nicolas_Cage_Deauville_2013.jpg", 
-    "https://2.bp.blogspot.com/-FG5qhZYlA9c/UNcj6FBBEgI/AAAAAAAACGE/LYOKb7XmUkU/s1600/Nicolas-Cage.jpg", 
-    "https://www.abruzzo24ore.tv/img/3bcda8f2aed2c8f1fdea1c020dadcf39/w/1024/h/576/scale/160579.jpg", 
-    "http://3.bp.blogspot.com/--eU2d6DyULI/UOlsIric3dI/AAAAAAAALHE/8jxR5F7erbc/s1600/Nicolas-Cage-nicolas-cage-26969948-1989-1300.jpg", 
-    "https://live.staticflickr.com/1/131983774_7eb586770f_b.jpg",
-    "https://images.uncyc.org/pt/1/11/Nicolascage_(14).jpg", 
-    "http://1.bp.blogspot.com/-uFkL5_XciyU/UOlsvnMBx4I/AAAAAAAALHU/vnRj0ehmGYg/s1600/tumblr_l31p8iOeC61qa06e2o1_500.jpg",
-    "http://1.bp.blogspot.com/-nXhiQU_2DtA/Uaxya_RUJRI/AAAAAAAAGaU/M6fNyE12zeM/s1600/Stringy+Mullet.jpg",
-    "http://www.internerdz.com.br/wp-content/uploads/2019/09/Nicolas-Cage-backgrounds-ultra-hd.png", 
-    "https://c4.wallpaperflare.com/wallpaper/46/357/191/movie-face-off-nicolas-cage-wallpaper-thumb.jpg", 
-    "https://i0.wp.com/www.cinezapping.com/wp-content/uploads/2010/08/Nicolas-Cage-ne-Lapprendista-stregone.jpg", 
-    "https://www.talkingdrugs.org/sites/default/files/images/cage-wicker-man.jpg", 
-    "https://c4.wallpaperflare.com/wallpaper/325/762/565/nicolas-cage-wallpaper-preview.jpg",
-    "https://multiglom.files.wordpress.com/2015/01/wildcage01.jpg", 
-    "https://4.bp.blogspot.com/_N91xwP-lKvw/TFjR4YQU1tI/AAAAAAAABk8/0VBlXRL3VBk/s1600/the+rock.jpg",
-    "https://i0.wp.com/www.cinezapping.com/wp-content/uploads/2010/08/Nicolas-Cage-in-Ghost-Rider.jpg", 
-    "https://cdn.pastemagazine.com/www/system/images/photo_albums/nicolas-cage/large/02-cage-valleygirl.jpg", 
-    "http://ll-media.tmz.com/2016/12/08/fast-times-memba-05-480w.jpg", 
-    "https://external-preview.redd.it/ACjjH16xtR9P1GBvLx-ykZdZ1W5RA8CSDInnSbBgwWk.jpg?auto=webp&s=ca4bd365e5dfebd9c8c3b7d7be0dc782702862e1", 
-    "https://www.slashfilm.com/img/gallery/nicolas-cage-couldnt-land-a-david-lynch-cameo-for-the-unbearable-weight-of-massive-talent/the-nicolas-cagedavid-lynch-reunion-we-deserved-1650027163.jpg",
-    "https://i1.wp.com/film-book.com/wp-content/uploads/2017/04/nicolas-cage-windtalkers-01-600x350.jpg", 
-    "https://multiglom.files.wordpress.com/2015/01/the-rock-1996.jpg", 
-    "https://musingsfromus.com/wp-content/uploads/2012/09/Gone-In-60-Seconds-2000-ScreenShot-088.jpg", 
-    "http://assets.acasatv.ro/assets/acasatv/2010/05/07/image_galleries/2130/vampirii-fac-valva-la-hollywood-galerie-foto_12.jpg", 
-    "https://multiglom.files.wordpress.com/2015/01/birdy.png", 
-    "https://musingsfromus.com/wp-content/uploads/2012/07/National-Treasure-2004-ScreenShot-121.jpg", 
-    "http://3.bp.blogspot.com/-rng145WCKPQ/T6mi8M8Ho5I/AAAAAAAAAEc/CaZfH8dVLGA/s1600/WeatherMan06.jpg", 
-    "http://de.web.img3.acsta.net/r_1280_720/medias/nmedia/18/35/51/53/18458294.jpg", 
-    "https://www.episodi.fi/wp-content/uploads/nicolas-cage-world-trade-center.jpg", 
-    "http://1.bp.blogspot.com/-WZRyOwsfE-8/U1bVZbeMK9I/AAAAAAAAAMY/JEe1Ie4G5fE/s1600/nicolas-cage-fu-manchu-grindhouse.jpg", 
-    "https://multiglom.files.wordpress.com/2015/01/badlieu02.jpg", 
-    "https://2.bp.blogspot.com/-1LyVzMxNm7Q/W83_4TYmZKI/AAAAAAAAAIY/sjkzpcFyoFYVJ_ousNs2u5AGETVXhGLdwCLcBGAs/s1600/Nic%252BCage.png", 
-    "https://www.yam-mag.com/wp-content/uploads/2012/05/nicholas-cage-kick-ass.jpg", 
+    "https://www.hellabrunn.de/fileadmin/_processed_/d/8/csm_wasserschwein-tierpark-hellabrunn-amerika-tierlexikon_f6feb22d52.jpg", //sitting 3
+    "https://www.savacations.com/wp-content/uploads/2021/02/Blog-Capybara-Pantanal-Brazil3.jpg", //sitting 4
+    "https://img.freepik.com/premium-photo/cute-capybara-farm-is-taking-bath_361141-902.jpg", //smiling 
+    "https://people.com/thmb/ovi1vkp7e_cTTPC5LglB_Ii83n0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(149x0:151x2)/capybara-1-300-dbd2c51946de4b989723201dac1f20ff.jpg", //screaming
+    "https://static01.nyt.com/images/2022/06/28/science/14tb-giantrodents/14tb-giantrodents-superJumbo.jpg", //stretching
+    "https://upload.wikimedia.org/wikipedia/commons/8/8e/Yellow-headed_caracara_%28Milvago_chimachima%29_on_capybara_%28Hydrochoeris_hydrochaeris%29.JPG", //with birb
+    "https://www.thealexandriazoo.com/images/animals/Capybara02.jpg",//thinking 
+    "https://www.jungleisland.com/wp-content/uploads/2022/07/capybara-gallery-4.jpg", //eating
+    "https://www.dartmoorzoo.org.uk/wp-content/uploads/2020/10/Capybara-1.png", //sitting 5
+    "https://images.foxtv.com/static.fox35orlando.com/www.fox35orlando.com/content/uploads/2021/06/764/432/Capybara-on-Gatorland-Flamingo-Island.jpg?ve=1&tl=1",//with flamingo 
+    "https://api.time.com/wp-content/uploads/2016/06/dog-capybara-friends.jpg?quality=85&w=1092", //with dog
+    "https://www.highparkzoo.ca/wp-content/uploads/2021/03/Capybara-site-sm.jpg",//double sitting
+    "https://critter.science/wp-content/uploads/2018/09/capybara1a-scaled.jpg", //sitting 6
+    "http://taronga.org.au/sites/default/files/content/header-images/header-tz-whats-on-capybaras-compressed-2880x1400.jpg", //eating 2
+    "https://gifts.worldwildlife.org/gift-center/Images/large-species-photo/large-capybara-photo.jpg", //face 
+    "https://www.rainforest-alliance.org/wp-content/uploads/2021/06/capybara-square-1.jpg.optimal.jpg", //stare
+    "https://app.digitickets.co.uk/userfiles/products/caybaras.1200x630.jpg",//looking for something
+    "https://www.thesprucepets.com/thmb/vg_tWh3npV1iZr24seq2_5RNN7g=/2119x0/filters:no_upscale():strip_icc()/GettyImages-590084935-57fc0b545f9b586c35c9daae.jpg", //ambush
+    "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F037%2F615%2Fcover2.jpg",//smiling2
+    "https://creativepark.canon/images/contents/CNT-0027974/CNT-0027974_detail-01_s@2x.jpg", //paper
+    "https://zooboise.org/content/uploads/2015/07/dreamstime_15775602.jpg", //water
+    "https://i.ytimg.com/vi/ClVV3BU2R28/hqdefault.jpg", //ok he pull up
+    "https://preview.redd.it/az3nid9sc9i81.jpg?width=640&crop=smart&auto=webp&s=46237c467cfb665dcc1ec92c80a5a49ee58fdfa5", //sing
 ]
 
 function getNic() {
